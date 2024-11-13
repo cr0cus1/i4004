cc37a1bf3b57c0932688bad352cb9a74bf9704b0
silver_punk <silvercrocus@proton.me>
2024-11-13 20:22:38 +0100

    I've started implementation of cpu's i4004 assembler.

--- i4004.c
+++ i4004.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <string.h>
 #include "i4004.h"
+#include "assembler.h"
+
+void cpu_execute_opcode(Cpu *cpu_unit) {
+   // here will be func for assembler(lex);
+}
 
 void cpu_init_env(Cpu *cpu_unit) {
     memset(cpu_unit->stack, 0, sizeof(cpu_unit->stack));
@@ -22,6 +27,7 @@ void cpu_run() {
                 cpu_unit.cycles_num += 1;
             }
             else {
+//                cpu_execute_opcode(&cpu_unit);
             }
         }
     }
--- i4004.h
+++ i4004.h
@@ -1,7 +1,7 @@
 #ifndef I4004_H_
 #define I4004_H_
 
-typedef struct Cpu{
+typedef struct Cpu {
     int stack[3];
     int registers[16];
     int accumulator;
@@ -12,5 +12,6 @@ typedef struct Cpu{
 void cpu_run();
 void cpu_init_env(Cpu *cpu_unit);
 void cpu_reset_env();
+void cpu_execute_opcode(Cpu *cpu_unit);
 
 #endif
